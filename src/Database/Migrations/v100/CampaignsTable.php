3edfc5322f22066264d8b476fd163599434950e1
phillmac <phillmac@users.noreply.github.com>
2019-09-23 11:57:33 +1000

    Add group foreign keys

--- src/Database/Migrations/v100/CampaignsTable.php
+++ src/Database/Migrations/v100/CampaignsTable.php
@@ -15,7 +15,9 @@ class CampaignsTable extends Migration
         if (!$this->schema->hasTable('campaigns')) {
             $this->schema->create('campaigns', function (Blueprint $table) {
                 $table->increments('id');
+                $table->unsignedInteger('group_id')->default(null)->nullable();
                 $table->json('metadata')->nullable();
+                $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade')->onUpdate('cascade');
 
                 $table->softDeletes();
                 $table->timestamps();
--- src/Database/Migrations/v100/MailingListsTable.php
+++ src/Database/Migrations/v100/MailingListsTable.php
@@ -15,7 +15,9 @@ class MailingListsTable extends Migration
         if (!$this->schema->hasTable('mailing_lists')) {
             $this->schema->create('mailing_lists', function (Blueprint $table) {
                 $table->increments('id');
+                $table->unsignedInteger('group_id')->default(null)->nullable();
                 $table->json('metadata')->nullable();
+                $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade')->onUpdate('cascade');
 
                 $table->softDeletes();
                 $table->timestamps();

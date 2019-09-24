d24eb7e409c543371f1b4ae391d2997d8de74098
phillmac <phillmac@users.noreply.github.com>
2019-09-24 13:47:37 +1000

    Add description field

--- src/Database/Migrations/v100/MailingListsTable.php
+++ src/Database/Migrations/v100/MailingListsTable.php
@@ -20,6 +20,7 @@ class MailingListsTable extends Migration
             $this->schema->create('mailing_lists', function (Blueprint $table) {
                 $table->increments('id');
                 $table->unsignedInteger('group_id')->default(null)->nullable();
+                $table->string('description', 255);
                 $table->json('metadata')->nullable();
                 $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade')->onUpdate('cascade');
 

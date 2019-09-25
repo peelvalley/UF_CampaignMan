2310fe460e607039add4dba2f00a7436f27a2e66
phillmac <phillmac@users.noreply.github.com>
2019-09-25 19:12:18 +1000

    Fix sprinkle name

--- routes/mailing-lists.php
+++ routes/mailing-lists.php
@@ -4,33 +4,33 @@ use UserFrosting\Sprinkle\Core\Util\NoCache;
 
 
 $app->group('/mailing_lists', function () {
-    $this->get('', 'UserFrosting\Sprinkle\Ems\Controller\MailingListController:pageList')
+    $this->get('', 'UserFrosting\Sprinkle\CampaignMan\Controller\MailingListController:pageList')
     ->setName('uri_events');
 
-    $this->get('/create', 'UserFrosting\Sprinkle\Ems\Controller\MailingListController:pageCreate');
+    $this->get('/create', 'UserFrosting\Sprinkle\CampaignMan\Controller\MailingListController:pageCreate');
 
-    $this->get('/ml/{ml_id}', 'UserFrosting\Sprinkle\Ems\Controller\MailingListController:pageInfo');
+    $this->get('/ml/{ml_id}', 'UserFrosting\Sprinkle\CampaignMan\Controller\MailingListController:pageInfo');
 
 })->add('authGuard')->add(new NoCache());
 
 
 
 $app->group('/api/events', function () {
-    $this->get('', 'UserFrosting\Sprinkle\Ems\Controller\MailingListController:getList');
+    $this->get('', 'UserFrosting\Sprinkle\CampaignMan\Controller\MailingListController:getList');
 
-    $this->post('', 'UserFrosting\Sprinkle\Ems\Controller\MailingListController:create');
+    $this->post('', 'UserFrosting\Sprinkle\CampaignMan\Controller\MailingListController:create');
 
-    $this->delete('/ml/{ml_id}', 'UserFrosting\Sprinkle\Ems\Controller\MailingListController:delete');
+    $this->delete('/ml/{ml_id}', 'UserFrosting\Sprinkle\CampaignMan\Controller\MailingListController:delete');
 
-    $this->get('/ml/{ml_id}', 'UserFrosting\Sprinkle\Ems\Controller\MailingListController:getInfo');
+    $this->get('/ml/{ml_id}', 'UserFrosting\Sprinkle\CampaignMan\Controller\MailingListController:getInfo');
 
-    $this->put('/ml/{ml_id}', 'UserFrosting\Sprinkle\Ems\Controller\MailingListController:updateInfo');
+    $this->put('/ml/{ml_id}', 'UserFrosting\Sprinkle\CampaignMan\Controller\MailingListController:updateInfo');
 
-    $this->put('/ml/{ml_id}/{field}', 'UserFrosting\Sprinkle\Ems\Controller\MailingListController:updateField');
+    $this->put('/ml/{ml_id}/{field}', 'UserFrosting\Sprinkle\CampaignMan\Controller\MailingListController:updateField');
 
 
 })->add('authGuard')->add(new NoCache());
 
 $app->group('/modals/events', function () {
-    $this->get('/confirm-delete', 'UserFrosting\Sprinkle\Ems\Controller\MailingListController:getModalConfirmDelete');
+    $this->get('/confirm-delete', 'UserFrosting\Sprinkle\CampaignMan\Controller\MailingListController:getModalConfirmDelete');
 })->add('authGuard')->add(new NoCache());

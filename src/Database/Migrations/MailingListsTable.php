a74477e7ef297645d73ecba964e1f9b82181ede0
phillmac <phillmac@users.noreply.github.com>
2019-09-30 12:53:12 +1000

    Add MailingQueueTable migration

---
+++ src/Database/Migrations/MailingListsTable.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace UserFrosting\Sprinkle\CampaignMan\Database\Migrations\v100;
+
+use Illuminate\Database\Schema\Blueprint;
+use UserFrosting\Sprinkle\Core\Database\Migration;
+
+class MailingQueueTable extends Migration
+{
+
+    /**
+    * {@inheritdoc}
+    */
+    public function up()
+    {
+        if (!$this->schema->hasTable('mailing_queue')) {
+            $this->schema->create('mailing_queue', function (Blueprint $table) {
+                $table->increments('id');
+                $table->json('data')->nullable();
+
+                $table->softDeletes();
+                $table->timestamps();
+
+                $table->engine = 'InnoDB';
+                $table->collation = 'utf8mb4_unicode_520_ci';
+                $table->charset = 'utf8mb4';
+            });
+        }
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    public function down()
+    {
+        if ($this->schema->hasTable('mailing_queue')) {
+            $this->schema->drop('mailing_queue');
+        }
+    }
+}

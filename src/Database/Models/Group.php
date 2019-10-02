c1127a3fbcfd27d52dd25d69c959a4171a398091
phillmac <phillmac@users.noreply.github.com>
2019-10-02 13:10:50 +1000

    Conditionaly extend UserProfile sprinkle group if present

--- src/Database/Models/Group.php
+++ src/Database/Models/Group.php
@@ -5,49 +5,29 @@ namespace UserFrosting\Sprinkle\CampaignMan\Database\Models;
 
 
 if (class_exists('UserFrosting\Sprinkle\UserProfile\Database\Models\Group')) {
-    class Group extends UserFrosting\Sprinkle\UserProfile\Database\Models\Group
-    {
-        /**
-         * Eloquent relation to the mailing_lists table.
-         */
-        public function mailingLists()
-        {
-            return $this->hasMany($classMapper->getClassMapping('mailing_list'), 'group_id');
-        }
-
-        /**
-         * Delete the group's mailing lists when deleting the main model.
-         *
-         * @return void
-         */
-        public function delete()
-        {
-            $this->mailingLists()->delete();
-            parent::delete();
-        }
-    }
+    class_alias('My\ArrayObject', 'UserFrosting\Sprinkle\CampaignMan\Database\Models\CoreGroup');
 } else {
-    class Group extends UserFrosting\Sprinkle\Account\Database\Models\Group
-    {
-        /**
-         * Eloquent relation to the mailing_lists table.
-         */
-        public function mailingLists()
-        {
-            return $this->hasMany($classMapper->getClassMapping('mailing_list'), 'group_id');
-        }
-
-        /**
-         * Delete the group's mailing lists when deleting the main model.
-         *
-         * @return void
-         */
-        public function delete()
-        {
-            $this->mailingLists()->delete();
-            parent::delete();
-        }
-    }
+    class_alias('UserFrosting\Sprinkle\Account\Database\Models\Group', 'UserFrosting\Sprinkle\CampaignMan\Database\Models\CoreGroup');
 }
 
+class Group extends CoreGroup
+{
+    /**
+     * Eloquent relation to the mailing_lists table.
+     */
+    public function mailingLists()
+    {
+        return $this->hasMany($classMapper->getClassMapping('mailing_list'), 'group_id');
+    }
 
+    /**
+     * Delete the group's mailing lists when deleting the main model.
+     *
+     * @return void
+     */
+    public function delete()
+    {
+        $this->mailingLists()->delete();
+        parent::delete();
+    }
+}

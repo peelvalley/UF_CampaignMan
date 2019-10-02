ae653f11dab3350852aceb00c7bc6c7d0e4fb933
phillmac <phillmac@users.noreply.github.com>
2019-10-02 12:13:10 +1000

    Fix typo in route uri

--- routes/group.php
+++ routes/group.php
@@ -4,7 +4,7 @@ use UserFrosting\Sprinkle\Core\Util\NoCache;
 
 
 $app->group('/group', function () {
-    $this->get('/g/{slug}.mailing_lists', 'UserFrosting\Sprinkle\CampaignMan\Controller\GroupMailingController:pageMailingLists')
+    $this->get('/g/{slug}/mailing_lists', 'UserFrosting\Sprinkle\CampaignMan\Controller\GroupMailingController:pageMailingLists')
     ->setName('uri_group_mailing_lists');
 
 

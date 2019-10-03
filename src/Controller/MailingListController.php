176fc468b49ac40ff89107322100b03e4fed6efd
phillmac <phillmac@users.noreply.github.com>
2019-10-03 11:36:45 +1000

    Cleanup & fix classes

--- src/Controller/MailingListController.php
+++ src/Controller/MailingListController.php
@@ -7,14 +7,10 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use UserFrosting\Fortress\Adapter\JqueryValidationAdapter;
 use UserFrosting\Fortress\RequestSchema;
-use UserFrosting\Fortress\RequestSchema\RequestSchemaRepository;
+use UserFrosting\Fortress\RequestDataTransformer;
 use UserFrosting\Fortress\ServerSideValidator;
 use UserFrosting\Sprinkle\Core\Controller\SimpleController;
-use UserFrosting\Sprinkle\Core\Util\EnvironmentInfo;
-use UserFrosting\Sprinkle\FormGenerator\Form;
-use UserFrosting\Sprinkle\UserProfile\Util\UserProfileHelper;
 use UserFrosting\Support\Exception\ForbiddenException;
-use UserFrosting\Support\Repository\Loader\YamlFileLoader;
 
 
 

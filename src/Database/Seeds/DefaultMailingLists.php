90cbad8a0257c9ef97778a211da1e686ae7ee950
phillmac <phillmac@users.noreply.github.com>
2019-10-03 10:49:21 +1000

    Add DefaultMailingLists seed

---
+++ src/Database/Seeds/DefaultMailingLists.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace UserFrosting\Sprinkle\CampaignMan\Database\Seeds;
+
+use UserFrosting\Sprinkle\Core\Database\Seeder\BaseSeed;
+use UserFrosting\Sprinkle\CampaignMan\Database\Models\Group;
+use UserFrosting\Sprinkle\CampaignMan\Database\Models\MailingList;
+
+use UserFrosting\Sprinkle\Core\Facades\Seeder;
+
+
+class DefaultMailingLists extends BaseSeed
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function run()
+    {
+       foreach (Group::doesnthave('mailingLists')->get() as $group) {
+            $group->mailingLIsts->save( new MailingList([
+                'description' => 'Default mailing list'
+            ]));
+       }
+    }
+}

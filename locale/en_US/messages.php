a3b541cc671895bdbd58ab3d70287eed71a1b23c
phillmac <phillmac@users.noreply.github.com>
2019-10-03 16:32:59 +1000

    Add translation

--- locale/en_US/messages.php
+++ locale/en_US/messages.php
@@ -4,5 +4,9 @@ return [
     'MAILING_LIST' => [
         1 => 'Mailing List',
         2 => 'Mailing Lists'
+    ],
+    'SUBSCRIBER' => [
+        1 => 'Subscriber',
+        2 => 'Subscribers'
     ]
 ];

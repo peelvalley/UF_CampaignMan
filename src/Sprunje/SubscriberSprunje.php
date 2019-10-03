7ccd4c82919293e399e109df0398b17e906f8bb2
phillmac <phillmac@users.noreply.github.com>
2019-10-03 13:47:25 +1000

    Debug

--- src/Sprunje/SubscriberSprunje.php
+++ src/Sprunje/SubscriberSprunje.php
@@ -35,7 +35,10 @@ class SubscriberSprunje extends Sprunje
      */
     protected function baseQuery()
     {
-        throw new \Exception(print_r([$this->$classMapper], TRUE));
+        throw new \Exception(print_r([
+            'this' => $this,
+            'classMapper' => $this->$classMapper
+        ], TRUE));
         return $this->$classMapper->getClassMapping('subscriber');
     }
 }

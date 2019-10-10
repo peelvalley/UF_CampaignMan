64a0c618cf347fc46e84e15a78aaefb06fe5e18e
phillmac <phillmac@users.noreply.github.com>
2019-10-10 13:25:49 +1100

    Add subscribe modal

--- routes/group.php
+++ routes/group.php
@@ -11,5 +11,8 @@ $app->group('/groups', function () {
 $app->group('/api/groups', function () {
     $this->get('/g/{slug}/mailing_lists', 'UserFrosting\Sprinkle\CampaignMan\Controller\GroupMailingController:getMailingLists');
     $this->post('/g/{slug}/mailing_lists/ml/{ml_slug}', 'UserFrosting\Sprinkle\CampaignMan\Controller\GroupMailingController:createSubscriber');
+})->add('authGuard')->add(new NoCache());
 
+$app->group('/modals/groups', function () {
+    $this->get('/g/{slug}/mailing_lists/ml/{ml_slug}/subscribe', 'UserFrosting\Sprinkle\CampaignMan\Controller\GroupMailingController:getModalCreateSubscription');
 })->add('authGuard')->add(new NoCache());

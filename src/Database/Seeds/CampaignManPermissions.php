a4dd32dd6963fa3e70b999b78a985d408f52904a
phillmac <phillmac@users.noreply.github.com>
2019-10-11 13:24:25 +1100

    Debug

--- src/Database/Seeds/CampaignManPermissions.php
+++ src/Database/Seeds/CampaignManPermissions.php
@@ -98,8 +98,8 @@ class CampaignManPermissions extends BaseSeed
         $roleGroupAdmin = Role::where('slug', 'group-admin')->first();
         if ($roleGroupAdmin) {
             $roleGroupAdmin->permissions()->syncWithoutDetaching([
-                $permissions['group_create_mailing_lists']->id,
-                $permissions['group_create_subscription']->id,
+                //$permissions['group_create_mailing_lists']->id,
+                //$permissions['group_create_subscription']->id,
                 $permissions['group_view_mailing_lists']->id,
                 $permissions['group_view_mailing_list']->id
 

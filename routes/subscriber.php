457221974ffb0da842cc9e31308cc6cc5be303b1
phillmac <phillmac@users.noreply.github.com>
2019-10-18 11:15:26 +1100

    Fix typo

--- routes/subscriber.php
+++ routes/subscriber.php
@@ -7,13 +7,13 @@ use UserFrosting\Sprinkle\Core\Util\NoCache;
 $app->group('/api/subscribers', function () {
     $this->post('', 'UserFrosting\Sprinkle\CampaignMan\Controller\SubscriberController:create');
 
-    $this->delete('/s/{subscruber_id}', 'UserFrosting\Sprinkle\CampaignMan\Controller\SubscriberController:delete');
+    $this->delete('/s/{subscriber_id}', 'UserFrosting\Sprinkle\CampaignMan\Controller\SubscriberController:delete');
 
-    $this->get('/s/{subscruber_id}', 'UserFrosting\Sprinkle\CampaignMan\Controller\SubscriberController:getInfo');
+    $this->get('/s/{subscriber_id}', 'UserFrosting\Sprinkle\CampaignMan\Controller\SubscriberController:getInfo');
 
-    $this->put('/s/{subscruber_id}', 'UserFrosting\Sprinkle\CampaignMan\Controller\SubscriberController:updateInfo');
+    $this->put('/s/{subscriber_id}', 'UserFrosting\Sprinkle\CampaignMan\Controller\SubscriberController:updateInfo');
 
-    $this->put('/s/{subscruber_id}/{field}', 'UserFrosting\Sprinkle\CampaignMan\Controller\SubscriberController:updateField');
+    $this->put('/s/{subscriber_id}/{field}', 'UserFrosting\Sprinkle\CampaignMan\Controller\SubscriberController:updateField');
 
 
 })->add('authGuard')->add(new NoCache());

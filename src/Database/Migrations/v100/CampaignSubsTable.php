daf0c01710a9057410f5997d83d3208c76453fd7
phillmac <phillmac@users.noreply.github.com>
2019-10-22 13:01:40 +1100

    Use subscriptions table instead of subscriber

--- src/Database/Migrations/v100/CampaignSubsTable.php
+++ src/Database/Migrations/v100/CampaignSubsTable.php
@@ -18,20 +18,20 @@ class CampaignSubsTable extends Migration
     */
     public function up()
     {
-        if (!$this->schema->hasTable('campaign_subscriber')) {
-            $this->schema->create('campaign_subscriber', function (Blueprint $table) {
+        if (!$this->schema->hasTable('campaign_subscriber_subscription')) {
+            $this->schema->create('campaign_subscriber_subscription', function (Blueprint $table) {
                 $table->increments('id');
                 $table->integer('campaign_id')->unsigned();
-                $table->integer('subscriber_id')->unsigned();
+                $table->integer('subscriber_subscription_id')->unsigned();
                 $table->string('status', 15);
                 $table->json('data')->nullable();
-
 
                 $table->timestamps();
 
                 $table->index('status');
                 $table->foreign('campaign_id')->references('id')->on('campaigns')->onDelete('cascade')->onUpdate('cascade');
-                $table->foreign('subscriber_id')->references('id')->on('subscribers')->onDelete('cascade')->onUpdate('cascade');
+                $table->foreign('subscriber_subscription_id')->references('id')->on('subscriber_subscription')->onDelete('cascade')->onUpdate('cascade');
+                $table->unique(['campaign_id', 'subscriber_subscription_id']);
 
                 $table->engine = 'InnoDB';
                 $table->collation = 'utf8mb4_unicode_520_ci';
@@ -45,8 +45,8 @@ class CampaignSubsTable extends Migration
     */
     public function down()
     {
-        if ($this->schema->hasTable('campaign_subscriber')) {
-            $this->schema->drop('campaign_subscriber');
+        if ($this->schema->hasTable('campaign_subscriber_subscription')) {
+            $this->schema->drop('campaign_subscriber_subscription');
         }
     }
 }
